d24f996bc0dab4c26442133b0981f44de5cf85b8
Rohit Kumar <142965701+rohit2212kr@users.noreply.github.com>
2026-07-09 10:13:44 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
+    unordered_map<int, int> mp;
     int fib(int n) {
         if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        if(mp.find(n) != mp.end()){
+            return mp[n];
+        }
+        int ans = fib(n-1) + fib(n-2);
+        mp[n] = ans;
+        return ans;
     }
 };
