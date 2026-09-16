5b35464d1e82d1efc93840a169af68651a2a2d04
Rohit Kumar <142965701+rohit2212kr@users.noreply.github.com>
2026-09-17 01:51:49 +0530

    Time: 105 ms (5.07%), Space: 19.3 MB (5.09%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,53 +2,41 @@ class Solution {
 public:
 
     bool dfs(vector<vector<char>>& board, int row, int col, int i, int j){
-        if(i < 0 || i >= row || j < 0 || j >= col)
-            return false;
+        if(i<0 || i>row-1 || j<0 || j> col-1) return false;
 
-        if(board[i][j] == 'X' || board[i][j] == 'V')
-            return false;
+        if(board[i][j] == 'X' || board[i][j] == 'v') return true;
 
-        if(i == 0 || i == row-1 || j == 0 || j == col-1)
-            return true;
+        board[i][j] = 'v';
 
-        board[i][j] = 'V';
+       bool a =  dfs(board, row, col, i+1, j);
+       bool b =  dfs(board, row, col, i-1, j);
+       bool c =  dfs(board, row, col, i, j+1);
+       bool d =  dfs(board, row, col, i, j-1);
 
-        bool up = dfs(board, row, col, i-1, j);
-        bool down = dfs(board, row, col, i+1, j);
-        bool left = dfs(board, row, col, i, j-1);
-        bool right = dfs(board, row, col, i, j+1);
-
-        return up || down || left || right;
+       return a && b && c && d;
     }
 
     void solve(vector<vector<char>>& board) {
         int row = board.size();
         int col = board[0].size();
-
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-
+        
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
                 if(board[i][j] == 'O'){
-
-                    bool boundary = dfs(board, row, col, i, j);
-
-                    if(boundary){
-                        // V → O
-                        for(int x = 0; x < row; x++){
-                            for(int y = 0; y < col; y++){
-                                if(board[x][y] == 'V')
-                                    board[x][y] = 'O';
+                    if(dfs(board, row, col, i, j)){
+                        for(int x = 0; x<row; x++){
+                            for(int y = 0; y<col; y++){
+                                if(board[x][y] == 'v') board[x][y] = 'X';
                             }
                         }
                     }
                     else{
-                        // V → X
-                        for(int x = 0; x < row; x++){
-                            for(int y = 0; y < col; y++){
-                                if(board[x][y] == 'V')
-                                    board[x][y] = 'X';
+                        for(int x = 0; x<row; x++){
+                            for(int y = 0; y<col; y++){
+                                if(board[x][y] == 'v') board[x][y] = 'O';
                             }
                         }
+
                     }
                 }
             }
